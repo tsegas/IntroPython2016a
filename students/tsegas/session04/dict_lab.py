d9d94110a3a3eab3911c4db7f74ab3cbb56bd61e
tsegas <tsega000@gmail.com>
2016-03-10 14:15:31 -0800

    update dict_lab project

--- students/tsegas/session04/dict_lab.py
+++ students/tsegas/session04/dict_lab.py
@@ -8,5 +8,14 @@ print("print the dict",my)
 my['fruit'] = 'mango'
 print("print the dict",my)
 
+print("print the dictionary keys",my.keys())
+print("print the dictionary values",my.values())
+
+a = "chocolate" in my
+print("is chocolate a key in the dictionary?",a)
+
+b = "fruit" in my
+print("is Mango a value in the dictionary?",b)
+
 cpy_my = my.copy()
 print("print the dict",cpy_my)
